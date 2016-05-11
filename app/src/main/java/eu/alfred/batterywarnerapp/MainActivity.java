c288259522259d67d1a5835334524686a85a9b4d
Gerrit Klasen <klasen@ascora.de>
2016-05-11 16:36:45 +0200

    back btn

--- app/src/main/java/eu/alfred/batterywarnerapp/MainActivity.java
+++ app/src/main/java/eu/alfred/batterywarnerapp/MainActivity.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import eu.alfred.api.proxies.interfaces.ICadeCommand;
 import eu.alfred.batterywarnerapp.actions.BatteryStatusQuery;
 import eu.alfred.ui.AppActivity;
+import eu.alfred.ui.BackToPAButton;
 import eu.alfred.ui.CircleButton;
 
 public class MainActivity extends AppActivity implements ICadeCommand {
@@ -24,7 +25,11 @@ public class MainActivity extends AppActivity implements ICadeCommand {
         //setContentView(eu.alfred.batterywarnerapp.R.layout.activity_main);
 
         circleButton = (CircleButton) findViewById(R.id.voiceControlBtn);
-        circleButton.setOnTouchListener(new CircleTouchListener());
+        circleButton.setOnTouchListener(new MicrophoneTouchListener());
+
+        backToPAButton = (BackToPAButton) findViewById(R.id.backControlBtn);
+        backToPAButton.setOnTouchListener(new BackTouchListener());
+
     }
 
     @Override
